ca1ba69a9afa5a3a81809c7c5f6c612947a7e07c
Lulu Nwenyi <oluchinwenyi@gmail.com>
2022-02-25 09:35:53 +0100

    procfile and requirements update

--- api/config.py
+++ api/config.py
@@ -1,11 +1,11 @@
 #----- IMPORTS -----#
 
 import os
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 from pathlib import Path
 
-#dotenv_path = Path('.env')
-#load_dotenv(dotenv_path=dotenv_path)
+dotenv_path = Path('.env')
+load_dotenv(dotenv_path=dotenv_path)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
