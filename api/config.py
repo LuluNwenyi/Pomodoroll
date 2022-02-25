eb77eb450abf50ab061c62d0e5d5cbf914845415
Lulu Nwenyi <oluchinwenyi@gmail.com>
2022-02-25 09:11:16 +0100

    no dotenv

--- api/config.py
+++ api/config.py
@@ -4,8 +4,8 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-dotenv_path = Path('.env')
-load_dotenv(dotenv_path=dotenv_path)
+#dotenv_path = Path('.env')
+#load_dotenv(dotenv_path=dotenv_path)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
